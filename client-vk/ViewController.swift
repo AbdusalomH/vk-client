e3e80c79f9b4037c256aba2c49b1431173c98de3
Abdusalom <>
2022-06-25 18:52:42 +0500

    build(app): initialize new project

---
+++ client-vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-vk
+//
+//  Created by Mac on 6/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
