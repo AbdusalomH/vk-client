63c53db9af5dba1db6c037225557b94428007f42
Abdusalom <>
2022-08-04 11:44:06 +0500

    feat(group): add network layer to group

--- client-vk/Screens/Groups/GroupsVC.swift
+++ client-vk/Screens/Groups/GroupsVC.swift
@@ -8,12 +8,14 @@
 import UIKit
 import SkeletonView
 
+
+
+
+
 class GroupsVC: UIViewController {
     
-    var isShownSkeleton: Bool = false
-        
-    var groups: [Group] = []
-    var filteredGroup: [Group] = []
+    private var viewModel = ViewModel()
+
     
     lazy var groupTable: UITableView = {
         
@@ -45,6 +47,7 @@ class GroupsVC: UIViewController {
         setupContraints()
         setupSkeleton()
         fetchGroups()
+
         
     }
     
@@ -62,32 +65,25 @@ class GroupsVC: UIViewController {
     
     
     func setupSkeleton() {
-        if !isShownSkeleton {
+        if !viewModel.isShownSkeleton {
             groupTable.isSkeletonable = true
             groupTable.showAnimatedGradientSkeleton(usingGradient: .init(baseColor: .gray), animation: nil, transition: .crossDissolve(0.5))
             groupTable.startSkeletonAnimation()
-            isShownSkeleton = true
+            viewModel.isShownSkeleton = true
         }
     }
     
     
     private func fetchGroups() {
         
-    let service = GroupsApi()
-        
-        if isShownSkeleton {
-            service.fetchGroups { result in
-                switch result {
-                case .success(let groups):
-                    self.groupTable.stopSkeletonAnimation()
-                    self.groupTable.hideSkeleton(reloadDataAfter: true, transition: .crossDissolve(0.5))
-                    self.groups = groups
-                    self.groupTable.reloadData()
+        if viewModel.isShownSkeleton {
+            viewModel.fetchGroup {
+                self.groupTable.stopSkeletonAnimation()
+                self.groupTable.hideSkeleton(reloadDataAfter: true, transition: .crossDissolve(0.2))
+                self.groupTable.reloadData()
                     
-                case .failure(let error):
-                    self.presentAlert(title: "Something went wrong", body: error.rawValue, button: "OK")
-                    print(error.rawValue)
-                }
+            } failure: { err in
+                print(err)
             }
         }
     }
@@ -96,19 +92,19 @@ class GroupsVC: UIViewController {
 extension GroupsVC: UITableViewDataSource, UITableViewDelegate, SkeletonTableViewDataSource  {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if !filteredGroup.isEmpty {
-            return filteredGroup.count
+        if !viewModel.filteredGroup.isEmpty {
+            return viewModel.filteredGroup.count
         } else {
-            return groups.count
+            return viewModel.groups.count
         }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: GroupsCell.reuseID, for: indexPath) as! GroupsCell
-        let groupsRespons = groups[indexPath.row]
+        let groupsRespons = viewModel.groups[indexPath.row]
 
-        if !filteredGroup.isEmpty {
-            let filteredResponse = self.filteredGroup[indexPath.row]
+        if !viewModel.filteredGroup.isEmpty {
+            let filteredResponse = self.viewModel.filteredGroup[indexPath.row]
             cell.configureCell(filteredResponse)
         } else {
             cell.configureCell(groupsRespons)
@@ -121,8 +117,8 @@ extension GroupsVC: UITableViewDataSource, UITableViewDelegate, SkeletonTableVie
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if !filteredGroup.isEmpty {
-            let selectedGroup = filteredGroup[indexPath.row]
+        if !viewModel.filteredGroup.isEmpty {
+            let selectedGroup = viewModel.filteredGroup[indexPath.row]
             let groupDetail = UINavigationController(rootViewController:  GroupsDetailsVC(title: selectedGroup.name, image: selectedGroup.photo200))
             if let groupSheetMenu = groupDetail.sheetPresentationController {
                 
@@ -136,7 +132,7 @@ extension GroupsVC: UITableViewDataSource, UITableViewDelegate, SkeletonTableVie
             
         } else {
             
-            let selectedGroup = groups[indexPath.row]
+            let selectedGroup = viewModel.groups[indexPath.row]
             
             let groupDetail = UINavigationController(rootViewController:  GroupsDetailsVC(title: selectedGroup.name, image: selectedGroup.photo200))
             if let groupSheetMenu = groupDetail.sheetPresentationController {
@@ -156,12 +152,12 @@ extension GroupsVC: UISearchResultsUpdating, UISearchBarDelegate {
     
     func updateSearchResults(for searchController: UISearchController) {
         guard let text = searchController.searchBar.text, !text.isEmpty else {return}
-        filteredGroup = groups.filter({$0.name.lowercased().contains(text.lowercased())})
+        viewModel.filteredGroup = viewModel.groups.filter({$0.name.lowercased().contains(text.lowercased())})
         groupTable.reloadData()
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        filteredGroup.removeAll()
+        viewModel.filteredGroup.removeAll()
         groupTable.reloadData()
     }
 }
