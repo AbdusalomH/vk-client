2454536c7110bf8aadd528da7bd4ca291a56142a
Abdusalom <>
2022-08-29 10:58:48 +0500

    refactor(friends): add new tableview cell, add style to cell

--- client-vk/Screens/Friends/FriendsCell.swift
+++ client-vk/Screens/Friends/FriendsCell.swift
@@ -14,6 +14,13 @@ class FriendsCell: UITableViewCell {
     
     static let reuseID = "FriendsCell"
     
+    let backview: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
     let friendImage: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
@@ -42,37 +49,62 @@ class FriendsCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        contentView.backgroundColor = UIColor.clear
+        backgroundColor = UIColor.clear
+        
+        backview.layer.cornerRadius = 15
+        backview.clipsToBounds = true
+        
+        friendImage.layer.cornerRadius = 40
+        friendImage.contentMode = .scaleAspectFit
+        
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOpacity = 0.3
+        layer.shadowOffset = .zero
+    }
+    
     
     func configure(_ friend: Friend) {
         nameLabel.text = friend.firstName
         guard let imageURL = URL(string: friend.photo100) else {return}
         friendImage.sd_setImage(with: imageURL)
     }
+
     
     
     func setupViews() {
-        
+        contentView.addSubview(backview)
         contentView.addSubview(friendImage)
         contentView.addSubview(nameLabel)
         
         contentView.isSkeletonable  = true
         friendImage.isSkeletonable  = true
         nameLabel.isSkeletonable    = true
-
+        
     }
     
     func setupContraints() {
         
         NSLayoutConstraint.activate([
-            friendImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
-            friendImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-            friendImage.heightAnchor.constraint(equalToConstant: 50),
-            friendImage.widthAnchor.constraint(equalToConstant: 50),
+            
+            backview.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            backview.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+            backview.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            backview.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            
+            
+            friendImage.topAnchor.constraint(equalTo: backview.topAnchor, constant: 20),
+            friendImage.leadingAnchor.constraint(equalTo: backview.leadingAnchor, constant: 20),
+            friendImage.bottomAnchor.constraint(equalTo: backview.bottomAnchor, constant: -20),
+            friendImage.heightAnchor.constraint(equalToConstant: 80),
+            friendImage.widthAnchor.constraint(equalToConstant: 80),
    
+            
             nameLabel.leadingAnchor.constraint(equalTo: friendImage.trailingAnchor, constant: 20),
-            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
-            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
-            nameLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20),
+            nameLabel.topAnchor.constraint(equalTo: backview.topAnchor, constant: 20),
+            nameLabel.trailingAnchor.constraint(equalTo: backview.trailingAnchor, constant: -20),
+            nameLabel.bottomAnchor.constraint(equalTo: backview.bottomAnchor, constant: -20),
         ])  
     }
 }
